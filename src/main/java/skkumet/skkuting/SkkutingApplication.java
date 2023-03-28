a2b25a509e9ca61bae277b6945806d11bc2a535e
17wook2 <alsdnr9999@g.skku.edu>
2023-03-28 17:42:31 +0900

    프로젝트 시작 #3
    스프링 백엔드 프로젝트를 시작

---
+++ src/main/java/skkumet/skkuting/SkkutingApplication.java
@@ -0,0 +1,13 @@
+package skkumet.skkuting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkkutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkkutingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skkumet/skkuting/SkkutingApplicationTests.java
@@ -0,0 +1,13 @@
+package skkumet.skkuting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkkutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
