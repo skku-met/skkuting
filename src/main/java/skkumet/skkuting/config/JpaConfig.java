38e6ee4a1c04a6603d0c2e875966aa75ede7b42b
wook2 <59720037+17wook2@users.noreply.github.com>
2023-04-03 00:05:37 +0900

    #5 auditing클래스 추가
    auditing을 위한 JPA 설정을 작성

---
+++ src/main/java/skkumet/skkuting/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package skkumet.skkuting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> Optional.of("skkuting contemporary");
+    }
+}
