2a9abca46ba050708168ea1bedf3ac0daec1f646
wook2 <59720037+17wook2@users.noreply.github.com>
2023-04-03 21:31:17 +0900

    모임 Read 구현 #7
    기본적인 모임 Read 메서드 구현완료.
    한 번에 모임에 관한 정보를 다 가져오는 메서드는 추후 UI와 화면부분이 완료되면, 필요한 데이터를 한번에 가져오도록 하는 전략이 필요해보임.

--- src/main/java/skkumet/skkuting/controller/MeetupController.java
+++ src/main/java/skkumet/skkuting/controller/MeetupController.java
@@ -1,15 +1,15 @@
 package skkumet.skkuting.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 import skkumet.skkuting.domain.UserAccount;
+import skkumet.skkuting.dto.MeetupDto;
 import skkumet.skkuting.dto.request.MeetupRequest;
 import skkumet.skkuting.repository.UserAccountRepository;
 import skkumet.skkuting.service.MeetupService;
+import skkumet.skkuting.service.UserAccountService;
 
 @RequiredArgsConstructor
 @RequestMapping("/meetup")
@@ -17,12 +17,21 @@ import skkumet.skkuting.service.MeetupService;
 public class MeetupController {
 
     private final MeetupService meetupService;
-    private final UserAccountRepository userAccountRepository;
+
+    @GetMapping
+    public Page<MeetupDto> meetUp(Pageable pageable){
+        return meetupService.getMeetups(pageable);
+    }
 
     @PostMapping
     public void postNewMeetup(@RequestBody MeetupRequest meetupRequest){
-        UserAccount userAccount = userAccountRepository.findById("test@g.skku.edu").get();
-        System.out.println(meetupRequest);
-        meetupService.saveMeetup(meetupRequest.toDto(userAccount));
+        meetupService.saveMeetup(meetupRequest);
     }
+
+    @PostMapping("/update")
+    public void updateMeetup(@RequestBody MeetupRequest meetupRequest) {
+
+    }
+
+
 }
