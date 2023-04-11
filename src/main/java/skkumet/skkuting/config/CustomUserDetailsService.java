5657fa10fc519a5e864896bfd0f03d9cb15e8f84
wook2 <59720037+17wook2@users.noreply.github.com>
2023-04-11 21:08:52 +0900

    시큐리티 버그 수정
    시큐리티 버그 수정

--- src/main/java/skkumet/skkuting/config/CustomUserDetailsService.java
+++ src/main/java/skkumet/skkuting/config/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import skkumet.skkuting.dto.UserAccountDto;
 import skkumet.skkuting.dto.UserAccountPrincipal;
@@ -12,15 +13,15 @@ import skkumet.skkuting.repository.UserAccountRepository;
 
 @RequiredArgsConstructor
 @Service
-public class CustomUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserAccountRepository userAccountRepository;
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return  userAccountRepository.findById(username)
+        return userAccountRepository.findById(username)
                 .map(UserAccountDto::from)
                 .map(UserAccountPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다." + username));
     }
+
 }
